8ebcb44a1f3d118edc69360ca42a38dff602921c
s921132820 <s921132820@gmail.com>
2025-03-04 11:51:53 +0900

    #5. Comment URUD

--- src/main/java/com/my/articles/dao/CommentDAO.java
+++ src/main/java/com/my/articles/dao/CommentDAO.java
@@ -1,9 +1,42 @@
 package com.my.articles.dao;
 
+import com.my.articles.entity.Article;
+import com.my.articles.entity.Comment;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Transactional
 public class CommentDAO {
+    @Autowired
+    EntityManager em;
+
+    public Long deleteComment(Long id) {
+        Comment comment = em.find(Comment.class, id);
+        em.remove(comment);
+        return comment.getArticle().getId();
+    }
+
+    public void insertComment(Long articleId, Comment comment) {
+        Article article = em.find(Article.class, articleId);
+        // 댓글 객체에 게시글 객체 추가
+        comment.setArticle(article);
+        // 게시글의 댓글 리스트에 comment 추가
+        article.getComments().add(comment);
+        // persist 선언해 놨으니까 article 저장
+        em.persist(article);
+    }
+
+    public Comment findByCommentId(Long commentId) {
+        return em.find(Comment.class, commentId);
+    }
+
+    public void updateComment(Comment comment) {
+        Comment original = em.find(Comment.class, comment.getId());
+        // dirty checking으로 값만 변경하면 저장 됨
+        original.setNickname(comment.getNickname());
+        original.setBody(comment.getBody());
+    }
 }
