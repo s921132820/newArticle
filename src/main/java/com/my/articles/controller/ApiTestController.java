66dac1f0010a71cea3fab9bae07d0ca4ee4dad82
s921132820 <s921132820@gmail.com>
2025-03-04 10:19:13 +0900

    #4. Comment API init

--- src/main/java/com/my/articles/controller/ApiTestController.java
+++ src/main/java/com/my/articles/controller/ApiTestController.java
@@ -5,9 +5,11 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ApiTestController {
@@ -22,5 +24,29 @@ public class ApiTestController {
         String info = dto.getUserid() + " / " + dto.getPassword();
         System.out.println("=========info : " + info);
         return ResponseEntity.status(HttpStatus.OK).body(info);
+    }
+
+    @PostMapping("getResponse")
+    @ResponseBody
+    public LoginDTO postResponse() {
+        return new LoginDTO("안유진", "1234");
+    }
+
+    @PostMapping("apiPostArrayTest")
+    public ResponseEntity<Map<String, String>> apiPostArrayTest(
+            @RequestBody List<LoginDTO> dtos
+            ) {
+        dtos.stream().forEach(x -> System.out.println(x));
+        Map<String, String> userData = new HashMap<>();
+        userData.put("id", dtos.get(1).getUserid());
+        userData.put("pw", dtos.get(1).getPassword());
+        return ResponseEntity.status(HttpStatus.OK).body(userData);
+    }
+
+    @DeleteMapping("/apiTest")
+    @ResponseBody
+    public String apiHttpMethodTest() {
+        String message = "Delete Mapping";
+        return message;
     }
 }
